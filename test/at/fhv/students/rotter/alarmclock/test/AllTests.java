ac74859c794c5ea003ab347f1ddfdd3f35298e40
Daniel Rotter <daniel.rotter@gmail.com>
2012-06-14 22:25:36 +0200

    Included all tests in AllTests-Testsuite

--- test/at/fhv/students/rotter/alarmclock/test/AllTests.java
+++ test/at/fhv/students/rotter/alarmclock/test/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TimeTest.class})
+@Suite.SuiteClasses({TimeTest.class, AlarmClockTest.class})
 public class AllTests {
 }
